03f1217f9d26aa128bc9abb0f5aa30fc7c1a7bba
bvogsDynatrace <bobby.vogler@dynatrace.com>
2023-05-02 16:19:40 -0600

    testing out new cards withing data page

--- biz-event-impact/src/app/pages/Data.tsx
+++ biz-event-impact/src/app/pages/Data.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Flex, Paragraph, DQLEditor, Grid, Heading } from "@dynatrace/strato-components-preview"
+import { Flex, Paragraph, DQLEditor, Grid, Heading, useCurrentTheme } from "@dynatrace/strato-components-preview"
+import { Borders, BoxShadows, Colors } from '@dynatrace/strato-design-tokens';
 import { useDQLQuery } from "../hooks/useDQLQuery";
 import { Card } from '../components/Card';
 
@@ -11,17 +12,42 @@ type DataProps = {
 
 export const Data = ({bizobj}:DataProps) => {
     const initialQuery = "fetch logs \n| summarize count(), by:bin(timestamp, 1m)";
+    const theme = useCurrentTheme();
 
     return(
-        <Flex>
+        <Flex style={{
+            border: Colors.Border.Neutral.Default,
+            borderRadius: Borders.Radius.Container.Subdued,
+            background: Colors.Background.Surface.Default,
+            boxShadow: BoxShadows.Surface.Raised.Rest,
+            width: '400px',
+            height: '120px',
+            alignItems: "center",
+            justifyContent: 'center',
+          }}>
             <Paragraph>{bizobj}</Paragraph>
-            <DQLEditor value={initialQuery} />
-
-
-            <Heading level={1}>Bobby Voglers Booking Overview</Heading>
-            <Grid gap={32} gridTemplateColumns={'2fr 2fr'}>
-                
+            {/* <DQLEditor value={initialQuery} /> */}
+            <Grid gap={64} gridTemplateColumns={'2fr 2fr'}>
+            
             </Grid>
+            <Card
+                href="/data"
+                inAppLink
+                imgSrc={theme === "light" ? "./assets/data.png" : "./assets/data_dark.png"}
+                name="Process results"
+            /> 
+            <Card
+                href="/data"
+                inAppLink
+                imgSrc={theme === "light" ? "./assets/data.png" : "./assets/data_dark.png"}
+                name="Process results"
+            /> 
+            <Card
+                href="/data"
+                inAppLink
+                imgSrc={theme === "light" ? "./assets/data.png" : "./assets/data_dark.png"}
+                name="Process results"
+            /> 
         </Flex>
     );
 }
