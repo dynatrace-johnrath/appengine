7b50d8af4d344e347f130f17cc200a2f2aeb1c55
bvogsDynatrace <bobby.vogler@dynatrace.com>
2023-05-02 15:58:14 -0600

    adding template to data file

--- biz-event-impact/src/app/pages/Data.tsx
+++ biz-event-impact/src/app/pages/Data.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { Flex, Paragraph, DQLEditor } from "@dynatrace/strato-components-preview"
+import { Flex, Paragraph, DQLEditor, Grid, Heading } from "@dynatrace/strato-components-preview"
 import { useDQLQuery } from "../hooks/useDQLQuery";
+import { Card } from '../components/Card';
 
 type DataProps = {
     /**Biz Event Attribute */
@@ -12,9 +13,15 @@ export const Data = ({bizobj}:DataProps) => {
     const initialQuery = "fetch logs \n| summarize count(), by:bin(timestamp, 1m)";
 
     return(
-        <Flex >
+        <Flex>
             <Paragraph>{bizobj}</Paragraph>
             <DQLEditor value={initialQuery} />
+
+
+            <Heading level={1}>Bobby Voglers Booking Overview</Heading>
+            <Grid gap={32} gridTemplateColumns={'2fr 2fr'}>
+                
+            </Grid>
         </Flex>
     );
 }
