ad5dac8371ec2c95d1c1c66ff755838b3601c0db
bvogsDynatrace <bobby.vogler@dynatrace.com>
2023-05-04 21:43:01 -0600

    Tried a bunch of stuff to get the @ to work

--- biz-event-impact/app.config.ts
+++ biz-event-impact/app.config.ts
@@ -7,9 +7,10 @@ const config: CliOptions = {
     version: '0.0.0',
     description: 'A starting project with routing, fetching Grail™ data, and charting',
     id: 'my.biz.event.impact',
-    scopes: [{ name: 'storage:metrics:read', comment: 'default template' }, 
+    scopes:[
+    {name: 'storage:metrics:read', comment: 'default template'}, 
     {name: 'storage:bizevents:read', comment: 'Read bizevents from Grail'},
-    { name: 'storage:buckets:read', comment: 'Read buckets from Grail' }]
+    {name: 'storage:buckets:read', comment: 'Read buckets from Grail'}]
   },
 };
 
