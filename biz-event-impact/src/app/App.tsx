a36da700817f5672d2aada98cc927bd30ebf1aec
Kyle Kowalski <kyle.kowalski@dynatrace.com>
2023-05-09 16:18:55 -0700

    remove homepage to simplify Lab Guide

--- biz-event-impact/src/app/App.tsx
+++ biz-event-impact/src/app/App.tsx
@@ -1,21 +1,62 @@
-import { Page } from "@dynatrace/strato-components-preview";
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Header } from "./components/Header";
-import { Home } from "./pages/Home";
-import {Data} from "./pages/Data"
+import React,{useState} from "react";
+import { Flex, Heading, Paragraph, FormField, TextInput, useCurrentTheme, Button } from "@dynatrace/strato-components-preview";
+import { ToastContainer,showToast } from "@dynatrace/strato-components-preview";
+import { Data } from "./pages/Data";
+
 
 export const App = () => {
+  const theme = useCurrentTheme();
+  const [email,setEmail] = useState('');
+  const [visable,setVisable] = useState(false);
+
+  function clickEvent(e) {
+    if (email == ''){
+      setVisable(false);
+      showToast({
+        title: 'Error',
+        type: 'warning',
+        message: 'Email must not be empty',
+        lifespan: 1000
+      });
+    }
+    else
+      setVisable(true);
+  };
+  
+  function resetEvent (e){
+    setVisable(false);
+    setEmail('');
+  };
+
   return (
-    <Page >
-      <Page.Header>
-        <Header />
-      </Page.Header>
-      <Page.Main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Page.Main>
-    </Page>
+    <>
+    <ToastContainer />
+
+    <Flex flexDirection="column" alignItems="center" padding={32}>
+      <img
+        src="./assets/Dynatrace_Logo.svg"
+        alt="Dynatrace Logo"
+        width={150}
+        height={150}
+        style={{ paddingBottom: 32 }}
+      ></img> 
+
+      <Heading>Dynatrace Non-Named Biz Event Impact Anaylsis App</Heading>
+      {!visable ? <>
+      <Paragraph>
+        This app is designed to assist a new App developer better understand the basics of the application 
+      </Paragraph>
+      <Paragraph>To run the app please enter an email address below</Paragraph>
+      </> : <Paragraph>Click the Button to search for a new user</Paragraph> }
+      <Flex>
+          <FormField label="">
+            {!visable ?<TextInput placeholder="john.smith@dynatraceapps.com" value={email} onChange={setEmail} /> :<TextInput readOnly value={email}/>}
+          </FormField>
+          {!visable ? <Button onClick={clickEvent} color="primary" variant="accent">Understand my Trades</Button> : <Button onClick={resetEvent} color="primary" variant="accent">Reset</Button>}
+      </Flex>
+
+    </Flex>
+    {visable ? <Data bizobj={email} loading={true}/> : <></>}
+    </>
   );
 };
