d2eefab093d2c7a1604d648883e934630ed81743
bvogsDynatrace <bobby.vogler@dynatrace.com>
2023-05-08 22:18:41 -0600

    adding comments and new queries

--- biz-event-impact/src/app/pages/Data.tsx
+++ biz-event-impact/src/app/pages/Data.tsx
@@ -18,41 +18,64 @@ export const Data = ({bizobj, loading}:DataProps) => {
     //still using static email for now
     const userEmail = "dawn.meza@yahoo.com"
 
-    //have to delcare the query before calling query function
-    //passing in userEmail to the query
-    const emailToId = `fetch bizevents | fields accountId, email | filter email == "` + userEmail + `" | summarize value = takeFirst(accountId)`;
-    
+
+
+    //------------------Query 1--------------------//
+    //Name: emailToId
+    //Purpose: pass in user email to get the users ID for future queries
+    //returns: Single number (ex: 56)
+
+    //have to delcare the query before calling query function (single quote or double quote or ` does not matter...)
+    //...only declaring this as a string before it is passed to the useDQLQuery() function
+
+    //passing in userEmail to the query to get account ID
+    const emailToIdDQL = `fetch bizevents | fields accountId, email | filter email == "` + userEmail + `" | summarize value = takeFirst(accountId)`;
     //run the query
-    const [emailQuery, isLoadingStarted] = useDQLQuery( emailToId, );
-    //turn the query result into a const number
-    const emailQueryId = Number(emailQuery?.records?.[0]?.value);
-    //use returned userId result in the next query
-    //
-    const [resultStarted, isLoadingFinsihed] = useDQLQuery(
-        'fetch bizevents, from:now()-24hr | filter accountId == ' + emailQueryId + ' | filter event.type == "easytrade.trade.buy" | summarize value = sum(amount)',
-        //'fetch bizevents, timeframe:"2022-01-20T00:00:00Z/2023-04-29T17:00:00Z"  | filter event.type == "booking.process.started" | summarize value = count()',
-      );
-    //return result of previous query as a number
-    const bookingStarted = Number(resultStarted?.records?.[0]?.value);
+    const [emailToIdReturn, isLoadingStarted] = useDQLQuery(emailToIdDQL);
+    //grab the number value from the query
+    const emailToId = Number(emailToIdReturn?.records?.[0]?.value);
+
+    //------------------Query 2--------------------//
+    //Name: stocksPurchased
+    //Purpose: use user ID from previous query to get dollar amount of stocks purchased in the past 24hrs
+    //Returns: Single number (ex: 11,573)
+
+    //another way of displaying DQL query using ` marks
+    const stocksPurchasedDQL = `fetch bizevents, from:now()-24hr 
+        | filter accountId == ` + emailToId + ` 
+        | filter event.type == "easytrade.trade.buy" 
+        | summarize value = sum(amount)`;
+    //run the query
+    const [stocksPurchasedReturn, isLoadingFinsihed] = useDQLQuery(stocksPurchasedDQL);
+    //grab the number value from the query
+    const stocksPurchased = Number(stocksPurchasedReturn?.records?.[0]?.value);
+    
+    //------------------Query 3--------------------//
+    //Name: 
+    //Purpose: 
+    //returns:
+
+
+
 
     return(
         <Flex flexDirection="column" alignItems="center" padding={32}>
         {/* Adding user ID (email in the future) to the page */}
         <Heading>User Email: {userEmail}</Heading>
-        <Heading>User Id: {emailQueryId}</Heading>
+        <Heading>User Id: {emailToId}</Heading>
             
             {/* <DQLEditor value={initialQuery} /> */}
             <Grid gap={32} gridTemplateColumns={'2fr 2fr'}>
                 <CardDQL
-                    value={bookingStarted}
+                    value={stocksPurchased}
                     chartLabel="Amount of stocks bought (24hrs)"
                     chartSuffix=""
                     //chart precision is what decimal you want the result to show
-                    chartPrecision={0}
+                    chartPrecision={2}
                     isLoading={isLoadingFinsihed}
                 />
                 <CardDQL
-                    value={bookingStarted}
+                    value={stocksPurchased}
                     chartLabel="Amount bought (24hrs)"
                     chartSuffix=""
                     //chart precision is what decimal you want the result to show
@@ -60,7 +83,7 @@ export const Data = ({bizobj, loading}:DataProps) => {
                     isLoading={isLoadingFinsihed}
                 />
                 <CardDQL
-                    value={bookingStarted}
+                    value={stocksPurchased}
                     chartLabel="Amount bought (24hrs)"
                     chartSuffix=""
                     //chart precision is what decimal you want the result to show
@@ -68,7 +91,7 @@ export const Data = ({bizobj, loading}:DataProps) => {
                     isLoading={isLoadingFinsihed}
                 />
                 <CardDQL
-                    value={bookingStarted}
+                    value={stocksPurchased}
                     chartLabel="Amount bought (24hrs)"
                     chartSuffix=""
                     //chart precision is what decimal you want the result to show
